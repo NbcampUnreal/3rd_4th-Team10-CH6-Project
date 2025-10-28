72b7902e8991025af843e044bb50f7adfda664db
HakunaMatata9909 <jkichul35@gmail.com>
2025-10-28 18:52:16 +0900

    초기 에셋 업로드

---
+++ Source/TenTenTown/ENumInputID.h
@@ -0,0 +1,13 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+
+UENUM(BlueprintType)
+enum class ENumInputID : uint8
+{
+	None UMETA(DisplayName = "None"),
+	Jump UMETA(DisplayName = "Jump"),
+	Dash UMETA(DisplayName = "Dash")
+};
