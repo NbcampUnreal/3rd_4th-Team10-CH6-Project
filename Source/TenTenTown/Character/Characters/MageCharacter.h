7c13e7ad70176f122f8fe3b9a7b51683869b54ad
dfdeer <dfdeer@naver.com>
2025-11-04 13:45:49 +0900

    추가(Character): 마법사 캐릭터 기본 이동 구현
    마법사 캐릭터의 기본적인 움직임 구현.
    대시(점멸) 구현.

---
+++ Source/TenTenTown/Character/Characters/MageCharacter.h
@@ -0,0 +1,69 @@
+//MageCharacter.cpp
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "InputAction.h"
+#include "GameFramework/Character.h"
+#include "MageCharacter.generated.h"
+
+struct FInputActionInstance;
+class UCameraComponent;
+class USpringArmComponent;
+class ATTTPlayerState;
+class UInputAction;
+class UInputMappingContext;
+class UGameplayAbility;
+enum class ENumInputID : uint8;
+class UAbilitySystemComponent;
+
+UCLASS()
+class TENTENTOWN_API AMageCharacter : public ACharacter
+{
+	GENERATED_BODY()
+
+public:
+	AMageCharacter();
+
+protected:
+	virtual void PossessedBy(AController* NewController) override;
+	virtual void OnRep_PlayerState() override;
+	
+public:	
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+
+protected:
+	//인풋 액션
+	UPROPERTY(EditDefaultsOnly,BlueprintReadOnly,Category="Inputs")
+	TObjectPtr<UInputMappingContext> IMC;
+	UPROPERTY(EditDefaultsOnly,BlueprintReadOnly,Category="Inputs")
+	TObjectPtr<UInputAction> MoveAction;
+	UPROPERTY(EditDefaultsOnly,BlueprintReadOnly,Category="Inputs")
+	TObjectPtr<UInputAction> LookAction;
+	UPROPERTY(EditDefaultsOnly,BlueprintReadOnly,Category="Inputs")
+	TObjectPtr<UInputAction> JumpAction;
+	UPROPERTY(EditDefaultsOnly,BlueprintReadOnly,Category="Inputs")
+	TObjectPtr<UInputAction> BlinkAction;
+
+	//인풋 액션 바인딩 함수
+	void Move(const FInputActionInstance& FInputActionInstance);
+	void Look(const FInputActionInstance& FInputActionInstance);
+	void ActivateGAByInputID(const FInputActionInstance& FInputActionInstance,ENumInputID InputID);
+	
+	//InputID, GA
+	UPROPERTY(EditDefaultsOnly,BlueprintReadOnly,Category = "GAS|EnputIDGAMap")
+	TMap <ENumInputID,TSubclassOf<UGameplayAbility>> InputIDGAMap;
+	
+	//기본 컴포넌트
+	UPROPERTY(VisibleAnywhere,BlueprintReadOnly,Category="Basic Components")
+	TObjectPtr<USpringArmComponent> SpringArmComponent;
+	UPROPERTY(VisibleAnywhere,BlueprintReadOnly,Category="Basic Components")
+	TObjectPtr<UCameraComponent> CameraComponent;
+	
+	//주요 캐싱
+	UPROPERTY(VisibleAnywhere,BlueprintReadOnly,Category="PlayerState")
+	TObjectPtr<ATTTPlayerState> PS;
+	
+	UPROPERTY(VisibleAnywhere,BlueprintReadOnly,Category="GAS|ASC")
+	TObjectPtr<UAbilitySystemComponent> ASC;
+};
