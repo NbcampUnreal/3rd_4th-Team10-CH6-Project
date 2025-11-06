7d36bf755c58b4f7730db3b9147a917b61d32342
USSA <ussa9905@gmail.com>
2025-11-06 17:51:35 +0900

    아래의 추가 커밋

---
+++ Source/TenTenTown/Enemy/TestEnemy/TestGold.h
@@ -0,0 +1,27 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "TestGold.generated.h"
+
+UCLASS()
+class TENTENTOWN_API ATestGold : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	// Sets default values for this actor's properties
+	ATestGold();
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category="Mesh")
+	UStaticMeshComponent* Mesh;
+	
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:	
+
+};
