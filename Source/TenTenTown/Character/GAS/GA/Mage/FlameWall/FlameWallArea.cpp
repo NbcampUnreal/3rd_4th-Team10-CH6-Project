246b411df170803720a441642cc07eb0123a2e40
dfdeer <dfdeer@naver.com>
2025-11-12 16:32:12 +0900

    추가(Character): 마법사 클래스 스킬B 대미지 구현
    + 자잘한 버그 수정

--- Source/TenTenTown/Character/GAS/GA/Mage/FlameWall/FlameWallArea.cpp
+++ Source/TenTenTown/Character/GAS/GA/Mage/FlameWall/FlameWallArea.cpp
@@ -1,5 +1,8 @@
 #include "FlameWallArea.h"
 
+#include "AbilitySystemComponent.h"
+#include "AbilitySystemGlobals.h"
+
 AFlameWallArea::AFlameWallArea()
 {
 	bReplicates = true;
@@ -14,6 +17,48 @@ AFlameWallArea::AFlameWallArea()
 void AFlameWallArea::Init(float InLifeTime)
 {
 	Lifetime = InLifeTime;
+}
+
+void AFlameWallArea::OnDamageZoneBeginOverlap(
+	UPrimitiveComponent* OverlappedComponent,
+	AActor* OtherActor, UPrimitiveComponent* OtherComp,
+	int32 OtherBodyIndex,
+	bool bFromSweep,
+	const FHitResult& SweepResult)
+{
+	if (!HasAuthority() || !OtherActor || !DotGE) return;
+	if (OtherActor == GetOwner()) return;
+
+	UAbilitySystemComponent* SourceASC = UAbilitySystemGlobals::GetAbilitySystemComponentFromActor(GetOwner());
+	UAbilitySystemComponent* TargetASC = UAbilitySystemGlobals::GetAbilitySystemComponentFromActor(OtherActor);
+
+	if (!SourceASC || !TargetASC) return;
+	
+	FGameplayEffectContextHandle Ctx = SourceASC->MakeEffectContext();
+	Ctx.AddSourceObject(this);
+	
+	FGameplayEffectSpecHandle Spec = SourceASC->MakeOutgoingSpec(DotGE, 1.f, Ctx);
+	if (!Spec.IsValid()) return;
+
+	Spec.Data->SetSetByCallerMagnitude(Tag_DoT, -DamagePerTick);
+	SourceASC->ApplyGameplayEffectSpecToTarget(*Spec.Data.Get(), TargetASC);
+}
+
+void AFlameWallArea::OnDamageZoneEndOverlap(
+	UPrimitiveComponent* OverlappedComponent,
+	AActor* OtherActor,
+	UPrimitiveComponent* OtherComp,
+	int32 OtherBodyIndex)
+{
+	if (!HasAuthority() || !OtherActor) return;
+
+	UAbilitySystemComponent* TargetASC = UAbilitySystemGlobals::GetAbilitySystemComponentFromActor(OtherActor);
+	if (!TargetASC) return;
+
+	if (!DotGrantedTags.IsEmpty())
+	{
+		TargetASC->RemoveActiveEffectsWithGrantedTags(DotGrantedTags);
+	}
 }
 
 void AFlameWallArea::BeginPlay()
--- Source/TenTenTown/Character/GAS/GA/Mage/FlameWall/FlameWallArea.h
+++ Source/TenTenTown/Character/GAS/GA/Mage/FlameWall/FlameWallArea.h
@@ -4,6 +4,8 @@
 #include "GameFramework/Actor.h"
 #include "FlameWallArea.generated.h"
 
+class UGameplayEffect;
+
 UCLASS()
 class TENTENTOWN_API AFlameWallArea : public AActor
 {
@@ -13,10 +15,24 @@ public:
 	AFlameWallArea();
 
 	void Init(float InLifeTime);
+
+	UFUNCTION()
+	void OnDamageZoneBeginOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult);
+	UFUNCTION()
+	void OnDamageZoneEndOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex);
 	
 protected:
 	virtual void BeginPlay() override;
 
+	UPROPERTY(EditDefaultsOnly, Category="Damage")
+	TSubclassOf<UGameplayEffect> DotGE;
+	UPROPERTY(EditDefaultsOnly, Category="Damage")
+	FGameplayTag Tag_DoT;
+	UPROPERTY(EditDefaultsOnly, Category="Damage")
+	FGameplayTagContainer DotGrantedTags;
+	UPROPERTY(EditDefaultsOnly, Category="Damage")
+	float DamagePerTick = 5.f;
+	
 private:
 	UPROPERTY(VisibleAnywhere)
 	TObjectPtr<USceneComponent> Root;
