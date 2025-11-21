116dbc0775a1dda5c00c8cb662e936427ef80d09
USSA <ussa9905@gmail.com>
2025-11-21 10:45:38 +0900

    [fix] Enemy 동기화 및 데미지 처리 로직 수정
    - TestGold에 bReplicates 및 ReplicateMovement 활성화.
    - GetDamage의 부호 확인 로직 추가.
    - DebugMessage에서 Health 변수 출력 오류 수정.
    - AEnemyBase의 OnDetection 함수에 아군 제외 로직 추가.
    - Enemy DataTable 갱신.

--- Source/TenTenTown/Enemy/Base/EnemyBase.cpp
+++ Source/TenTenTown/Enemy/Base/EnemyBase.cpp
@@ -87,7 +87,7 @@ void AEnemyBase::PostInitializeComponents()
 void AEnemyBase::OnDetection(UPrimitiveComponent* OverlappedComp, AActor* OtherActor, UPrimitiveComponent* OtherComp,
                              int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
 {
-	if (OtherActor && OtherActor != this && OtherActor->IsA<ACharacter>())
+	if (OtherActor && OtherActor != this && OtherActor->IsA<ACharacter>() && !OtherActor->IsA<AEnemyBase>())
 	{
 		if (!OverlappedPawns.Contains(OtherActor))
 		{
