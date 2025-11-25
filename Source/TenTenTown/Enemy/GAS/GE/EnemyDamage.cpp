c967655da6f59bb3ae3a0df081ec25adc672c59a
USSA <ussa9905@gmail.com>
2025-11-25 17:23:42 +0900

    추가커밋

--- Source/TenTenTown/Enemy/GAS/GE/EnemyDamage.cpp
+++ Source/TenTenTown/Enemy/GAS/GE/EnemyDamage.cpp
@@ -1,24 +1,19 @@
-// EnemyDamage.cpp
-#include "Enemy/GAS/AS/AS_EnemyAttributeSetBase.h"
 #include "EnemyDamage.h"
+#include "Enemy/GAS/AS/AS_EnemyAttributeSetBase.h"
 
 UEnemyDamage::UEnemyDamage()
 {
-	// GameplayEffect가 Instant로 실행되도록 설정
 	DurationPolicy = EGameplayEffectDurationType::Instant;
 
-	// Modifier 추가: Damage Attribute에 SetByCaller 값 적용
 	FGameplayModifierInfo ModifierInfo;
 
 	ModifierInfo.Attribute = UAS_EnemyAttributeSetBase::GetDamageAttribute();
 	ModifierInfo.ModifierOp = EGameplayModOp::Override;
 
-	// SetByCaller 사용
 	FSetByCallerFloat SetByCallerMagnitude;
 	SetByCallerMagnitude.DataTag = FGameplayTag::RequestGameplayTag(TEXT("Data.Enemy.Damage"));
 
 	ModifierInfo.ModifierMagnitude = FGameplayEffectModifierMagnitude(SetByCallerMagnitude);
 
-	// Modifier 배열에 추가
 	Modifiers.Add(ModifierInfo);
 }
