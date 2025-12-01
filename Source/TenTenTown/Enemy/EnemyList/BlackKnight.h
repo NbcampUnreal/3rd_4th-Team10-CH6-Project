0e6a4fdaafbe99adaad1fe63f0df08f3a40c0498
jin945 <wlsrnjs945@gmail,com>
2025-12-01 10:51:10 +0900

    추가(Enemy):BlackKnight 기초 구현

---
+++ Source/TenTenTown/Enemy/EnemyList/BlackKnight.h
@@ -0,0 +1,18 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Enemy/Base/EnemyBase.h"
+#include "BlackKnight.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class TENTENTOWN_API ABlackKnight : public AEnemyBase
+{
+	GENERATED_BODY()
+	ABlackKnight();
+	virtual void InitializeEnemy() override;
+};
