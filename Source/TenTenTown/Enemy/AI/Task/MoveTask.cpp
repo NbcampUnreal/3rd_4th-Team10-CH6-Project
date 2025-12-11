9f89223ec76f70cc4b2c894dc39bac9406a226a1
jin945 <wlsrnjs945@gmail,com>
2025-12-11 12:59:17 +0900

    수정(Enemy):몬스터 이동 중 스플라인 z축 회전 무시하도록 수정

--- Source/TenTenTown/Enemy/AI/Task/MoveTask.cpp
+++ Source/TenTenTown/Enemy/AI/Task/MoveTask.cpp
@@ -93,15 +93,17 @@ EStateTreeRunStatus UMoveTask::Tick(FStateTreeExecutionContext& Context, const f
 		FVector OffsetVector = RightVector * Actor->DistanceOffset;
 
 		FVector NewLocation = SplineLocation + OffsetVector;
-		
-		FRotator NewRotation = FRotationMatrix::MakeFromX(Direction).Rotator();
+
+		FRotator SplineRotation = Direction.Rotation();
+		FRotator NewRotation(0.f, SplineRotation.Yaw, 0.f);
+		//FRotator NewRotation = FRotationMatrix::MakeFromX(Direction).Rotator();
 		
 		if (!Actor->bIsFly)
 		{
 			//지상 몬스터 바닥 보정
 			FHitResult Hit;
-			FVector TraceStart = NewLocation + FVector(0.f, 0.f, 500.f);
-			FVector TraceEnd   = NewLocation - FVector(0.f, 0.f, 500.f);
+			FVector TraceStart = NewLocation + FVector(0.f, 0.f, 300.f);
+			FVector TraceEnd   = NewLocation - FVector(0.f, 0.f, 300.f);
 
 			FCollisionQueryParams Params;
 			Params.AddIgnoredActor(Actor);
